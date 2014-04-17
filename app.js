aa012f3bd19b150256baa0b0fc7b8a19bd2dfcec
Christina Kao <chris18@stanford.edu>
2014-04-17 02:12:30 -0700

    new structure

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var project = require('./routes/project');
+var milestone1 = require('./routes/milestone1');
+var milestone2 = require('./routes/milestone2');
+var milestone3 = require('./routes/milestone3');
+var milestone4 = require('./routes/milestone4');
+var milestone5 = require('./routes/milestone5');
+var milestone6 = require('./routes/milestone6');
+var milestone7 = require('./routes/milestone7');
+var milestone8 = require('./routes/milestone8');
 // Example route
 // var user = require('./routes/user');
 
@@ -37,8 +44,15 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/grid', index.viewGrid);
-app.get('/project/:id', project.view);
+app.get('/milestone1', milestone1.view);
+app.get('/milestone2', milestone2.view);
+app.get('/milestone3', milestone3.view);
+app.get('/milestone4', milestone4.view);
+app.get('/milestone5', milestone5.view);
+app.get('/milestone6', milestone6.view);
+app.get('/milestone7', milestone7.view);
+app.get('/milestone8', milestone8.view);
+
 // Example route
 // app.get('/users', user.list);
 
